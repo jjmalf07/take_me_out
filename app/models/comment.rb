ba3d1c9bf4c43a7d29de9730b60c3bbf1d14abde
jjmalf07 <jjmalf07@gmail.com>
2014-07-18 14:27:32 -0500

    Added comments table and thread to userdate show view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment < ActiveRecord::Base
+  belongs_to :userdate
+
+  belongs_to :user
+
+  validates :content, :presence => true, :uniqueness => { :scope => :user_id }
+
+  validates :user_id, :presence => true, :uniqueness => { :scope => :userdate_id }
+
+  validates :userdate_id, :presence => true
 end
--- app/models/userdate.rb
+++ app/models/userdate.rb
@@ -2,5 +2,6 @@ class Userdate < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
   has_many :matches
+  has_many :comments, :dependent => :destroy
   has_many(:users, { :through => :matches })
 end
